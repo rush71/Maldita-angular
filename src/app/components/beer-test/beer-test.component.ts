4a03f9d5ab52583e4e766a56beca6191ccdca736
DamianBarzolaweb <damianbarzolaweb@gmail.com>
2022-06-27 23:16:41 -0300

    beer test file ts

---
+++ src/app/components/beer-test/beer-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-beer-test',
+  templateUrl: './beer-test.component.html',
+  styleUrls: ['./beer-test.component.css']
+})
+export class BeerTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
