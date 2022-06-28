e783233b765d4afae8cc37e7a9a56c53a28fd2ba
DamianBarzolaweb <damianbarzolaweb@gmail.com>
2022-06-27 23:16:50 -0300

    navbar test ts

---
+++ src/app/components/navbar-test/navbar-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-test',
+  templateUrl: './navbar-test.component.html',
+  styleUrls: ['./navbar-test.component.css']
+})
+export class NavbarTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
