028acf73c3f091e298ba5e2c03cdcd80ced5a3fa
Francisco <francisco.montilla@live.com.ar>
2022-07-02 01:16:31 -0300

    navbar js

--- src/assets/js/navbar/navbar.js
+++ src/assets/js/navbar/navbar.js
@@ -8,6 +8,7 @@ closeMenu.addEventListener('click', close)
 function show (){
   mainMenu.style.display = 'flex'
   mainMenu.style.top = '0'
+  mainMenu.style.background ='red'
 }
 
 function close (){
